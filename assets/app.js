bda863215995b788e1e1a8aa2dfbc39f218ba1d4
Pape Samba SIDIBE <papesambas@yahoo.fr>
2024-07-25 19:19:28 +0200

    installation webpack, bootstrap et sass

--- assets/app.js
+++ assets/app.js
@@ -2,9 +2,23 @@ import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
-import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import TomSelect from 'tom-select';
+// You can specify which plugins you need
+//import { Tooltip, Toast, Popover } from 'bootstrap';
+
+ // loads the jquery package from node_modules
+ //import $ from 'jquery';
+ $(document).ready(function() {
+    console.log('jQuery is ready to use!');
+    //$('body').css('background-color', 'lightblue');
+});
+
